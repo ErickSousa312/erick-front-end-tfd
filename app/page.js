4844450fabaea98e21a1e16706cdba0bb7bc732c
Erik sales <58636303+ErickSousa312@users.noreply.github.com>
2023-07-21 01:28:55 -0300

    first commit login page
    feita a primeira versão da pagina de login

--- app/page.js
+++ app/page.js
@@ -1,12 +1,61 @@
-import Image from 'next/image'
-import styles from '@/styles/page.module.css'
+'use client'
 
-export default function Home() {
+import Image from 'next/image'
+import styled from 'styled-components';
+import styles from '@/styles/login/Login.module.css'
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import Btn from '@/components/login/Button/button'
+
+
+export default function Login(){
   return (
-    <main className={styles.main}>
-      <div>
-        oeieieieieieieieiei
-      </div>
-    </main>
+    <div className={styles.mainContainer}>
+    <div className={styles.forms}>
+        <h1 style={{fontWeight: "900", fontSize: "43px", marginTop:"2.5em", color:"rgb(0 55 255);"}}>LOGIN</h1>
+        <h3 className={styles.h3Forms}>Faça login para participar de votações <br /> ou<br /> Criar uma votação</h3>
+        <div className={styles.inputs}>
+        </div>
+        <div className={styles.OU}>
+          <div className={styles.linha2}></div>
+        </div>
+      <Btn 
+          shadow={"5px 5px 12px rgba(0,0,0,30%)"}
+          backgroundColor={'white'}
+          border={'none'}
+          margintop={"2em"}
+          adding={"10px 20px"}
+          cor={"black"}
+          servidor={"google"}>
+          <FcGoogle className={styles.iconGoogle} size={27}></FcGoogle> Entar com o google
+      </Btn>
+      <Btn 
+          shadow={"5px 5px 12px rgba(0,0,0,30%)"}
+          backgroundColor={'white'}
+          border={'none'}
+          margintop={"0em"}
+          adding={"10px 20px"}
+          cor={"black"}
+          servidor={"github"}>
+          <FaGithub className={styles.iconGoogle} size={27}></FaGithub> Entar com o google
+      </Btn>
+    </div>
+  </div>
+
   )
 }
+
+const StyledBackground = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center; 
+`;
+
+const StyleIMG = styled(Image)`
+background-size: cover;
+background-position: center;
+object-fit: cover;
+object-position: center;
+`
---
+++ components/login/Button/button.jsx
@@ -0,0 +1,24 @@
+import styles from '@/styles/login/button/Button.module.css'
+
+const Button = (props) => {
+    const estilo = props.className || styles.botao
+    return (
+        <button
+            className={styles.botao}
+            style={{
+                border: props.border,
+                backgroundColor: props.backgroundColor,
+                boxShadow: props.shadow,
+                width: props.width,
+                color: props.cor,
+                marginTop: props.margintop,
+                padding: props.padding,
+                position: props.position,
+                marginBottom: props.marginBottom,
+                margin: props.margin2
+            }}>
+            {props.children}
+        </button>
+    )
+}
+export default Button
